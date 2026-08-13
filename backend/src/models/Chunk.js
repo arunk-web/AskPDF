0207a5c2b2485bd6c022e01c32a6bf4a54d9765b
arunk-web <arunk02615@gmail.com>
2026-08-13 23:04:52 +0530

    built chunk schema

---
+++ backend/src/models/Chunk.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const chunkSchema = new mongoose.Schema(
+    {
+        documentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Document',
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+        embedding: {
+            type: [Number],
+            required: true,
+        },
+        chunkIndex: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps : true,
+    }
+)
+
+module.exports = mongoose.model('Chunk',chunkSchema);
