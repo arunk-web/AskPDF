2805abaf9ea84448a826fc7d9fae9c41b6c86707
arunk-web <arunk02615@gmail.com>
2026-08-14 18:42:28 +0530

    it helps to create chunks from a large text.

---
+++ backend/src/services/chunker.js
@@ -0,0 +1,18 @@
+
+const chunkText = (text) => {
+    const chunkSize = 1000;
+    const chunkOverlap = 200;
+
+    const chunks = [];
+    
+    let start = 0;
+    while(start < text.length){
+        const chunk = text.slice(start,start+chunkSize);
+        chunks.push(chunk);
+        start += chunkSize - chunkOverlap; 
+    }
+    return chunks;
+}
+
+module.exports = chunkText;
+
