0e1f2357847c612c3b8407d4fd29ee42d06433c1
arunk-web <arunk02615@gmail.com>
2026-08-16 21:31:13 +0530

    it helps to generate embedding values.

--- backend/src/services/embedder.js
+++ backend/src/services/embedder.js
@@ -1,15 +1,19 @@
 const {GoogleGenerativeAI} = require('@google/generative-ai')
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
-const model = genAI.getGenerativeModel({model: 'text-embedding-004'})
+const model = genAI.getGenerativeModel({model: 'gemini-embedding-001'})
 
 const generateEmbeddings = async (chunk) => {
     try{
-        const embeddedChunk = await model.embedContent(chunk);
-        const embeddingVector = embeddedChunk.embedding.values
-        return embeddingVector;
+        
+        const embeddedChunk = await model.embedContent(chunk,{
+            outputDimensionality: 768,
+        });
+
+        const embeddingValues = embeddedChunk.embedding.values
+        return embeddingValues;
     }
     catch(error){
-        throw error
+        throw error;
     }
 }
 
