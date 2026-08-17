9d565243c99691715955d0cd5e31d5eb9362c030
arunk-web <arunk02615@gmail.com>
2026-08-17 23:23:30 +0530

    main server file

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,17 @@
 //step 2
 const express = require('express')
 const cors = require('cors')
-
+const uploadRoutes = require('./routes/uploadRoutes');
+const chatRoutes = require('./routes/chatRoutes')
+const statusRoutes = require('./routes/statusRoutes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api',uploadRoutes);
+app.use('/api',chatRoutes)
+app.use('/api',statusRoutes);
 
 
 app.get('/', (req,res) => {
