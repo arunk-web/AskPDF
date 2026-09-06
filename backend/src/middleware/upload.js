d87cd8e4c1c784b1d3bc69ac982f1534e1de3fe8
arunk-web <arunk02615@gmail.com>
2026-09-06 11:25:17 +0530

    upload function

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -1,22 +1,6 @@
-const path = require('path');
 const multer = require('multer');
-const fs = require('fs');
 
-const uploadDir = path.join(__dirname, '../../uploads');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null,uploadDir);
-    },
-
-    filename:(req,file,cb)=>{
-        const newName = Date.now() + '-' + file.originalname;
-        cb(null,newName);
-    }
-});
+const storage = multer.memoryStorage();
 
 const filefilter = (req,file,cb) => {
     if(file.mimetype === 'application/pdf'){
