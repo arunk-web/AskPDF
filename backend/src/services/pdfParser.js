bdcdfa89346cb4725b2d58cceb83e7e92d1aaf24
arunk-web <arunk02615@gmail.com>
2026-09-06 11:24:59 +0530

    pdf parsing technique

--- backend/src/services/pdfParser.js
+++ backend/src/services/pdfParser.js
@@ -1,11 +1,8 @@
-const fs = require('fs')
 const pdfParse = require('pdf-parse');
 
-const extractTextfromPDF = async(filePath) => {
+const extractTextfromPDF = async(buffer) => {
     try{
-        const buffer = await fs.promises.readFile(filePath);
         const data = await pdfParse(buffer);
-
         return data.text;
     } catch(error) {
         console.error('PDF parsing failed:', error.message);
